d249e042f5e0c06c0bb931651759092cf2800a7c
Yunho Choi <choiking10@kaist.ac.kr>
2019-05-16 13:01:24 +0900

    add static and gitignore

---
+++ washu/static/README.txt
@@ -0,0 +1,9 @@
+Home page is index.html 
+You can browse from the home page to the other pages, 
+
+
+A Pen created at CodePen.io. You can find this one at https://codepen.io/anon/pen/GpNbVd.
+
+ A design for a sign-up/login form using tabs and floating form labels.
+
+Forked from [Eric](http://codepen.io/ehermanson/)'s Pen [Sign-Up/Login Form](http://codepen.io/ehermanson/pen/KwKWEv/).
---
+++ washu/static/js1/mydate.js
@@ -0,0 +1,25 @@
+//theDate
+var date = new Date();
+
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+
+if (month < 10) month = "0" + month;
+if (day < 10) day = "0" + day;
+
+var today = year + "-" + month + "-" + day;
+
+
+document.getElementById('theDate').value = today;
+
+$(function(){     
+  var d = new Date(),        
+      h = d.getHours(),
+      m = d.getMinutes();
+  if(h < 10) h = '0' + h; 
+  if(m < 10) m = '0' + m; 
+  $('input[type="time"][value="now"]').each(function(){ 
+    $(this).attr({'value': h + ':' + m});
+  });
+});
