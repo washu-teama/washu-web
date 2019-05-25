7ecc2eafa7175c7494c5c74c71790fc8591b4055
Yunho Choi <choiking10@kaist.ac.kr>
2019-05-25 16:41:33 +0900

    scheduling test

---
+++ washu/scheduling/run_scheduling.sh
@@ -0,0 +1,9 @@
+LOOP=$1
+DJANGO_HOME=$2
+while true; do
+    sleep LOOP &
+    TIME=$(date +%H:%M:%S)
+    printf '%s\n' $TIME
+    source $DJANGO_HOME/../venv/bin/activate && python3 $DJANGO_HOME/scheduling/check_event.py >> $HOME/log
+    wait # for sleep
+done
